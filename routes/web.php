3b56a92e327d13e53ea7856e6fe8d87308b7bf83
fathur <vathroh@gmail.com>
2021-12-20 13:53:12 +0700

    welcome

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect("http://www.osp1.my.id");
+  return view("welcome");
+    //return redirect("http://www.osp1.my.id");
 });
 
